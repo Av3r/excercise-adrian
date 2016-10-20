22178adca7f6d9d7d847649c1107bde7657576f9
Daniel Michalski <michalskidaniel2@gmail.com>
2016-10-20 17:37:02 +0200

    Refactor

--- src/main/java/object_programming_2/lab4/lamcomb/Main.java
+++ src/main/java/object_programming_2/lab4/lamcomb/Main.java
@@ -1,6 +1,5 @@
 package object_programming_2.lab4.lamcomb;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,33 +10,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Function<String, List<String>> flines = (filePath) -> {
-            try {
-                Path path = Paths.get(filePath);
-                return Files.readAllLines(path);
-            } catch (IOException e) {
-                return Lists.newArrayList();
-            }
-        };
-
-        Function<List<String>, String> join = strings -> Joiner.on("").join(strings);
-
-        Function<String, List<Integer>> collectInts = (s) -> {
-            s = s.replaceAll("[^0-9]+", " ");
-            Scanner scanner = new Scanner(s);
-            List<Integer> numbers = new ArrayList<>();
-            while (scanner.hasNextInt()) {
-                numbers.add(scanner.nextInt());
-            }
-            return numbers;
-        };
-
-        Function<List<Integer>, Integer> sum = ints -> ints.stream().mapToInt(Integer::intValue).sum();
+        Function<String, List<String>> flines = getFlinesFunction();
+        Function<List<String>, String> join = Main::getStringsFunction;
+        Function<String, List<Integer>> collectInts = getCollectIntsFunction();
+        Function<List<Integer>, Integer> sum = getSumFunction();
 
         String fname = System.getProperty("user.home") + "/LamComFile.txt";
         InputConverter<String> fileConv = new InputConverter<>(fname);
@@ -55,5 +37,37 @@ public class Main {
         InputConverter<List<String>> slistConv = new InputConverter<>(arglist);
         sumints = slistConv.convertBy(join, collectInts, sum);
         System.out.println(sumints);
+    }
+
+    private static Function<String, List<String>> getFlinesFunction() {
+        return (filePath) -> {
+            try {
+                Path path = Paths.get(filePath);
+                return Files.readAllLines(path);
+            } catch (IOException e) {
+                return Lists.newArrayList();
+            }
+        };
+    }
+
+    private static Function<String, List<Integer>> getCollectIntsFunction() {
+        return (s) -> {
+            s = s.replaceAll("[^0-9]+", " ");
+            Scanner scanner = new Scanner(s);
+            List<Integer> numbers = new ArrayList<>();
+            while (scanner.hasNextInt()) {
+                numbers.add(scanner.nextInt());
+            }
+            return numbers;
+        };
+    }
+
+    private static String getStringsFunction(List<String> strings) {
+        return strings.stream()
+                .collect(Collectors.joining());
+    }
+
+    static Function<List<Integer>, Integer> getSumFunction() {
+        return ints -> ints.stream().mapToInt(Integer::intValue).sum();
     }
 }
