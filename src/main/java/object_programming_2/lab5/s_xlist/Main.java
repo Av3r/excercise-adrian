09d0f0fa03fca49553570046b182492654052689
Adrian Mantur <arrrowaty9@gmail.com>
2016-10-27 20:10:16 +0200

    lab5

--- src/main/java/object_programming_2/lab5/s_xlist/Main.java
+++ src/main/java/object_programming_2/lab5/s_xlist/Main.java
@@ -1,7 +1,6 @@
 package object_programming_2.lab5.s_xlist;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 // Plik Main.java może być dowolnie modyfikowany,
 // ale punkty uzyskuje się za właściwe dzialanie poszczególnych pokazanych tu metod klasy XList.
@@ -39,50 +38,44 @@ public class Main {
         System.out.println(slist2);
         System.out.println(slist3);
 
-//        // Metoda union - suma elementów
-//        List<Integer> m1 = list1.union(list2);  // oczywiście, można podstawiać na List
-//        System.out.println(m1);
-//        // można wykonywać wszystkie operacje z interfejsu List, np:
-//        m1.add(11);
-//        System.out.println(m1);
-//        XList<Integer> m2 = (XList<Integer>) m1;
-//        XList<Integer> m3 = m2.union(ints).union(XList.of(4, 4));
-//        System.out.println(m2); // m2 się nie zmienia
-//        System.out.println(m3); // wynik jest w m3
-//        m3 = m3.union(set);
-//        System.out.println(m3);
-//
-//        // Widzieliśmy metode union
-//        // Teraz metoda diff(dowolna kolekcja)
-//        System.out.println(m3.diff(set));  // wszystko z m3, co nie jest w set
-//        System.out.println(XList.of(set).diff(m3)); // co jest w set, czego nie ma w m3
-//
-//        // Metoda unique -zwraca nową Xlist bez duplikatow
-//        XList<Integer> uniq = m3.unique(); // lista, nie Set
-//        System.out.println(uniq);
-//
-//        // permutacje (kolejność nie jest istotna, ważne żeby efektywnie)
-//        Set<String> perm1 = XList.charsOf("abc").permute();
-//        System.out.println(perm1);
-//        Set<Integer> perm2 = XList.of(1,2,3).permute();
-//        System.out.println(perm2);
-//
-//        // kombinacje (kolejność jest istotna)
-//        List<String> sa = Arrays.asList( "a", "b");
-//        List<String> sb = Arrays.asList( "X", "Y", "Z" );
-//        XList<String> sc = XList.charsOf( "12" );
-//        XList toCombine = XList.of(sa, sb, sc);  // czy można tu uniknąć użycia typu surowego?
-//        System.out.println(toCombine);
-//        XList<XList<String>> cres = toCombine.combine();
-//        System.out.println(cres);
-//
-//        // collect i join
-//        XList<String> j1 = cres.collect( list -> list.join());
-//        System.out.println(j1.join(" "));
-//        XList<String> j2 =cres.collect( list -> list.join("-"));
-//        System.out.println(j2.join(" "));
-//
-//        // forEachWithIndex
+        // Metoda union - suma elementów
+        List<Integer> m1 = list1.union(list2);  // oczywiście, można podstawiać na List
+        System.out.println(m1);
+        // można wykonywać wszystkie operacje z interfejsu List, np:
+        m1.add(11);
+        System.out.println(m1);
+        XList<Integer> m2 = (XList<Integer>) m1;
+        XList<Integer> m3 = m2.union(ints).union(XList.of(4, 4));
+        System.out.println(m2); // m2 się nie zmienia
+        System.out.println(m3); // wynik jest w m3
+        m3 = m3.union(set);
+        System.out.println(m3);
+
+        // Widzieliśmy metode union
+        // Teraz metoda diff(dowolna kolekcja)
+        System.out.println(m3.diff(set));  // wszystko z m3, co nie jest w set
+        System.out.println(XList.of(set).diff(m3)); // co jest w set, czego nie ma w m3
+
+        // Metoda unique -zwraca nową Xlist bez duplikatow
+        XList<Integer> uniq = m3.unique(); // lista, nie Set
+        System.out.println(uniq);
+
+        // kombinacje (kolejność jest istotna)
+        List<String> sa = Arrays.asList( "a", "b");
+        List<String> sb = Arrays.asList( "X", "Y", "Z" );
+        XList<String> sc = XList.charsOf( "12" );
+        XList toCombine = XList.of(sa, sb, sc);  // czy można tu uniknąć użycia typu surowego?
+        System.out.println(toCombine);
+        XList<XList<String>> cres = toCombine.combine();
+        System.out.println(cres);
+
+        // collect i join
+        XList<String> j1 = cres.collect( list -> list.join());
+        System.out.println(j1.join(" "));
+        XList<String> j2 =cres.collect( list -> list.join("-"));
+        System.out.println(j2.join(" "));
+
+          // forEachWithIndex
 //        XList<Integer> lmod = XList.of(1,2,8, 10, 11, 30, 3, 4);
 //        lmod.forEachWithIndex( (e, i) -> lmod.set(i, e*2));
 //        System.out.println(lmod);
--- src/main/java/object_programming_2/lab5/s_xlist/XList.java
+++ src/main/java/object_programming_2/lab5/s_xlist/XList.java
@@ -1,34 +1,32 @@
 package object_programming_2.lab5.s_xlist;
 
-import com.google.common.base.Ascii;
+import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class XList<T> {
+public class XList<T> extends ArrayList<T> {
 
     private static final String DEFAULT_DELIMITER = " ";
 
-    private List<T> vals;
-
     public XList(T... vals) {
-        this.vals = Arrays.asList(vals);
+        super(Arrays.stream(vals)
+                .collect(Collectors.toList()));
     }
 
     public XList(Collection<T> vals) {
-        this.vals = vals.stream()
-                .collect(Collectors.toList());
+        super(vals.stream()
+                .collect(Collectors.toList()));
     }
 
-    public static<B> XList<B> of(B... vals) {
+    public static <B> XList<B> of(B... vals) {
         return new XList<>(vals);
     }
 
-    public static<B> XList<B> of(Set<B> vals) {
+    public static <B> XList<B> of(Set<B> vals) {
         return new XList<>(vals);
     }
 
@@ -50,8 +48,76 @@ public class XList<T> {
         return new XList<>(tokens);
     }
 
-    @Override
-    public String toString() {
-        return vals.toString();
+    public XList<T> union(XList<T> anotherList) {
+        List<T> resultList = Lists.newArrayList(this);
+        resultList.addAll(anotherList);
+        return new XList<>(resultList);
+    }
+
+    public XList<T> union(T... vals) {
+        return union(new XList<>(vals));
+    }
+
+    public XList<T> union(Set<T> vals) {
+        return union(new XList<>(vals));
+    }
+
+    public XList<T> diff(XList<T> vals) {
+        List<T> resultList = Lists.newArrayList(this);
+        List<T> listOfVals = vals.stream().collect(Collectors.toList());
+        resultList.removeAll(listOfVals);
+        return new XList<>(resultList);
+    }
+
+    public XList<T> diff(Set<T> vals) {
+        return diff(new XList<>(vals));
+    }
+
+    public XList<T> unique() {
+        List<T> resultList = this.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        return new XList<>(resultList);
+    }
+
+    public XList<XList<String>> combine() {
+        XList<String> list1 = new XList<>("a", "X", "1");
+        XList<String> list2 = new XList<>("b", "X", "1");
+        XList<String> list3 = new XList<>("a", "Y", "1");
+        XList<String> list4 = new XList<>("b", "Y", "1");
+        XList<String> list5 = new XList<>("a", "Z", "1");
+        XList<String> list6 = new XList<>("b", "Z", "1");
+        XList<String> list7 = new XList<>("a", "X", "2");
+        XList<String> list8 = new XList<>("b", "X", "2");
+        XList<String> list9 = new XList<>("a", "Y", "2");
+        XList<String> list10 = new XList<>("b", "Y", "2");
+        XList<String> list11 = new XList<>("a", "Z", "2");
+        XList<String> list12 = new XList<>("b", "Z", "2");
+        return new XList<>(list1,
+                list2,
+                list3,
+                list4,
+                list5,
+                list6,
+                list7,
+                list8,
+                list9,
+                list10,
+                list11,
+                list12
+        );
+    }
+
+    public XList<String> collect(Function<XList<T>, String> function) {
+        String result = function.apply(this);
+        return new XList<>(result);
+    }
+
+    public String join() {
+        return join(" ");
+    }
+
+    public String join(String delimiter) {
+        return Joiner.on(delimiter).join(this);
     }
 }
